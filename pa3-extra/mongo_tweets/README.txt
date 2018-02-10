ca2185875d50a41f0fa6b82672106c69d180ecb3
Vajjhala <v2.surya@gmail.com>
2018-02-10 16:47:14 -0500

    removed pa3-extr

--- pa3-extra/mongo_tweets/README.txt
+++
@@ -1,9 +0,0 @@
-# mongo_tweets
-
-The "data" folder contains the raw data (including the csv data)
-
-The "output" folder contains the "map.html" file.
-
-
-To Run:
-python3 access_mongodb_databse.py > output/output.txt
--- pa3-extra/mongo_tweets/test/MongoDB_test.py
+++
@@ -1,8 +0,0 @@
-from pymongo import MongoClient
-from pprint import pprint
-client = MongoClient("localhost", 27017)
-print(client)
-MongoClient(host=['localhost:27017'], document_class=dict, tz_aware=False, connect=True)
-db=client.admin
-serverStatusResult=db.command("serverStatus")
-pprint(serverStatusResult)
